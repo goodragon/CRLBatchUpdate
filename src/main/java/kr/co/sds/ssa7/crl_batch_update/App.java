941bc0ac6b763a720fb86089e00088eeb3175c91
goodragon <kyjung76@gmail.com>
2014-04-06 07:51:54 +0900

    초기자료

---
+++ src/main/java/kr/co/sds/ssa7/crl_batch_update/App.java
@@ -0,0 +1,13 @@
+package kr.co.sds.ssa7.crl_batch_update;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
