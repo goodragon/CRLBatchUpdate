c0a1b8fea5f2ab83e13935ed593f323683a5d4e2
goodragon <kyjung76@gmail.com>
2014-04-27 21:30:52 +0900

    10개 ldap서버별 쓰레드 처리

--- src/main/java/kr/co/sds/ssa7/crl_batch_update/SimpleCRLMain.java
+++ src/main/java/kr/co/sds/ssa7/crl_batch_update/SimpleCRLMain.java
@@ -3,10 +3,26 @@ package kr.co.sds.ssa7.crl_batch_update;
 public class SimpleCRLMain {
 
 	public static void main(String[] args) {
-		// KICA
+		// 1.GPKI1
+		new SimpleCRLSearcherClient(".*cn=CA1310[0-9]{5},ou=GPKI.*", "ldap.gcc.go.kr").start();
+		// 2.GPKI2
+		new SimpleCRLSearcherClient(".*cn=CA1311[0-9]{5},ou=GPKI.*", "cen.dir.go.kr").start();
+		// 3.EPKI
+		new SimpleCRLSearcherClient(".*ou=CRL,ou=GPKI.*", "ldap.epki.go.kr").start();
+		// 4.GPKI(대검찰청)
+		new SimpleCRLSearcherClient(".*ou=crldp[0-9],ou=GPKI.*", "ldap.spo.go.kr").start();
+		// 5.GPKI(대법원)
+		new SimpleCRLSearcherClient(".*CN=CA9740[0-9]{5},OU=GPKI.*", "ldap.scourt.go.kr").start();
+		// 6.금융결제원
+		new SimpleCRLSearcherClient(".*o=yessign.*", "ds.yessign.or.kr").start();
+		// 7.증권전산
+		new SimpleCRLSearcherClient(".*o=SignKorea.*", "dir.signkorea.com").start();
+		// 8.무역정보
+		new SimpleCRLSearcherClient(".*o=TradeSign.*", "ldap.tradesign.net").start();
+		// 9.정보인증
 		new SimpleCRLSearcherClient(".*o=KICA.*", "ldap.signgate.com").start();
-		// CrossCert
+		// 10.전자인증
 		new SimpleCRLSearcherClient(".*o=CrossCert.*", "dir.crosscert.com").start();
-		// ETC
+		
 	}
 }
